fe9b210329d4fa48632a2485663becf0909d5cb4
Rubin Imeri <rubinimeri6@gmail.com>
2026-01-10 09:00:12 +0100

    feat: Fetch & show prayer times
    - Get prayer times from API on serverside
    - Filter & sort prayer times
    - Show prayer times visually

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,67 @@
-"use client";
-
+import { Card, CardContent } from "@/components/ui/card";
+import { Item, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
+import { PrayerTimes, PrayerTimesResponse } from "@/types/prayer-times";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  IconMoon,
+  IconSun,
+  IconSunrise,
+  IconSunset,
+  IconSunset2,
+} from "@tabler/icons-react";
 import { formatISO } from "date-fns";
-import { useEffect, useState } from "react";
 
-export default function Page() {
-  const [prayerTimes, setPrayerTimes] = useState(null);
+const HIDDEN_PRAYER_TIMES = ["Midnight", "Sunset", "Firstthird", "Lastthird"];
 
-  useEffect(() => {
-    const fetchPrayerTimes = () => {
-      fetch(
-        `${process.env.PRAYER_TIMES_API_BASE_URL}/timingsByCity/${formatISO(new Date())}?city=Skopje`,
-        {
-          method: "GET",
-        },
-      );
-    };
-  }, []);
+const prayerTimesIcons: Partial<Record<keyof PrayerTimes, React.JSX.Element>> =
+  {
+    Imsak: <IconSunrise className="text-blue-900" />,
+    Fajr: <IconSunrise className="text" />,
+    Sunrise: <IconSunrise className="text-orange-300" />,
+    Dhuhr: <IconSun className="text-yellow-600" />,
+    Asr: <IconSunset2 className="text-text-" />,
+    Maghrib: <IconSunset />,
+    Isha: <IconMoon />,
+  };
+
+async function getPrayerTimes(): Promise<PrayerTimesResponse> {
+  const response = await fetch(
+    `https://api.aladhan.com/v1/timingsByCity/${formatISO(new Date())}?city=Skopje&country=MK`,
+    {
+      method: "GET",
+    },
+  );
+
+  return await response.json();
+}
+
+const ERROR_CODE = 400;
+
+export default async function Page() {
+  const prayerTimes = await getPrayerTimes();
+
+  if (prayerTimes.code === ERROR_CODE || typeof prayerTimes.data === "string")
+    return <Card>Error getting prayer times!</Card>;
+
+  const filteredPrayerTimes = (
+    Object.entries(prayerTimes.data.timings) as [keyof PrayerTimes, string][]
+  )
+    .filter(([prayer]) => !HIDDEN_PRAYER_TIMES.includes(prayer))
+    .sort(([, a], [, b]) => a.localeCompare(b));
 
   return (
-    <main className="grid min-h-screen place-items-center">
-      <Card className="size-100 text-center">
-        <CardHeader>
-          <CardTitle>Quran Project</CardTitle>
-          <CardDescription>
-            Read the mercy Allah sent to humanity!
-          </CardDescription>
-        </CardHeader>
-        <CardContent></CardContent>
+    <main className="p-4">
+      <Card className="bg-card/30 max-w-sm mx-auto">
+        <CardContent>
+          {filteredPrayerTimes.map(([prayer, value]) => (
+            <Item key={prayer}>
+              <ItemMedia>{prayerTimesIcons[prayer]}</ItemMedia>
+              <ItemContent>
+                <ItemTitle>{prayer}</ItemTitle>
+              </ItemContent>
+              <h2 className="text-lg font-bold">{value}</h2>
+            </Item>
+          ))}
+        </CardContent>
       </Card>
     </main>
   );
